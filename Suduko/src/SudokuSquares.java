f3697e387418ec0a3f7106fda64c2c6c7dee7ce2
David <davidsjoblom@hotmail.se>
2021-06-30 13:59:16 +0200

    v3

--- Suduko/src/SudokuSquares.java
+++ Suduko/src/SudokuSquares.java
@@ -62,6 +62,86 @@ public class SudokuSquares {
 				Rnumbers.clear();
 			}
 
+			//--------
+			ArrayList<Integer> aColumn = new ArrayList<Integer>();
+			ArrayList<Integer> bColumn = new ArrayList<Integer>();
+			ArrayList<Integer> cColumn = new ArrayList<Integer>();
+			ArrayList<Integer> choosenColumn = new ArrayList<Integer>();
+			
+
+			aColumn.add(sudoku[1]);
+			aColumn.add(sudoku[10]);
+			aColumn.add(sudoku[19]);
+			aColumn.add(sudoku[2]);
+			aColumn.add(sudoku[11]);
+			aColumn.add(sudoku[20]);
+			
+			bColumn.add(sudoku[0]);
+			bColumn.add(sudoku[9]);
+			bColumn.add(sudoku[18]);
+			bColumn.add(sudoku[2]);
+			bColumn.add(sudoku[11]);
+			bColumn.add(sudoku[20]);
+			
+			cColumn.add(sudoku[0]);
+			cColumn.add(sudoku[9]);
+			cColumn.add(sudoku[18]);
+			cColumn.add(sudoku[1]);
+			cColumn.add(sudoku[10]);
+			cColumn.add(sudoku[19]);
+
+			for(int i = 0; i< 3;i++) {
+				
+				rand = r.nextInt(aColumn.size());
+				if(choosenColumn.contains(aColumn.get(rand))) {}
+					
+				else {
+					sudoku[27 + i * 9] = aColumn.get(rand);
+					choosenColumn.add(aColumn.get(rand));
+					if(bColumn.contains(aColumn.get(rand)) && bColumn.size() > 3) {
+						bColumn.remove(bColumn.indexOf(aColumn.get(rand))); //NOT SUPOSE TO REMOVE THESE, they are supose to be "locked" for the coming square.
+					}
+					if(cColumn.contains(aColumn.get(rand)) && cColumn.size() > 3) {
+						cColumn.remove(cColumn.indexOf(aColumn.get(rand)));
+					}
+					aColumn.remove(rand);
+				}
+				
+				rand = r.nextInt(bColumn.size());
+				if(choosenColumn.contains(bColumn.get(rand))) {}
+				
+				else {
+					sudoku[28 + i * 9] = bColumn.get(rand);
+					choosenColumn.add(bColumn.get(rand));
+					if(aColumn.contains(bColumn.get(rand)) && aColumn.size() > 3) {
+						aColumn.remove(aColumn.indexOf(bColumn.get(rand)));
+					}
+					if(cColumn.contains(bColumn.get(rand)) && cColumn.size() > 3) {
+						cColumn.remove(cColumn.indexOf(bColumn.get(rand)));
+					}
+					bColumn.remove(rand);
+				}
+				
+				rand = r.nextInt(cColumn.size());
+				if(choosenColumn.contains(cColumn.get(rand))) {}
+				
+				else {
+					sudoku[29 + i * 9] = cColumn.get(rand);
+					choosenColumn.add(cColumn.get(rand));
+					if(bColumn.contains(aColumn.get(rand)) && bColumn.size() > 3) {
+						bColumn.remove(bColumn.indexOf(aColumn.get(rand)));
+					}
+					if(aColumn.contains(cColumn.get(rand)) && aColumn.size() > 3) {
+						aColumn.remove(aColumn.indexOf(cColumn.get(rand)));
+					}
+					cColumn.remove(rand);
+				}
+			}
+			
+			
+			
+			
+			
 		printArray print = new printArray();
 		print.print(sudoku);
 	}
